704af756a32fa51b45387ed3ee96c53056355000
Ashin Siby <ashinsiby2024@mca.sjcetpalai.ac.in>
2023-06-16 21:18:36 +0530

    cycle 3 final changes and record

--- cycle 3/Main.java
+++ cycle 3/Main.java
@@ -1,21 +1,81 @@
 import java.util.Scanner;
-import java.util.Arrays;
-
-
-
-public class Employee{
-
-
-
-
+class Employee {
+    protected int empId;
+    protected String name;
+    protected double salary;
+    protected String address;
+    public Employee(int empId, String name, double salary, String address) {
+        this.empId = empId;
+        this.name = name;
+        this.salary = salary;
+        this.address = address;
+    }
 }
+class Teacher extends Employee {
+    private String department;
+    private String subjectsTaught;
 
+    public Teacher(int empId, String name, double salary, String address, String department, String subjectsTaught) {
+        super(empId, name, salary, address);
+        this.department = department;
+        this.subjectsTaught = subjectsTaught;
+    }
 
-public class Teacher extends Employee{
+   public void display() {         
+         System.out.println("Employee ID: " + empId);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+        System.out.println("Address: " + address);
+        System.out.println("Department: " + department);
+        System.out.println("Subjects Taught: " + subjectsTaught);
+        System.out.println("---------------------------");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         System.out.println("---------------------------");
+         System.out.println("Ashin Siby");
+      System.out.println("22MCA014");
+      System.out.println("09-06-2023");
+      System.out.println("---------------------------");
+        System.out.print("Enter the number of teachers: ");
+        int numTeachers = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
 
+          Teacher[] teachers = new Teacher[numTeachers];
 
+        for (int i = 0; i < numTeachers; i++) {
+            System.out.println("Enter details for Teacher " + (i + 1));
 
+            System.out.print("Employee ID: ");
+            int empId = scanner.nextInt();
+            scanner.nextLine();
 
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
 
+            System.out.print("Salary: ");
+            double salary = scanner.nextDouble();
+            scanner.nextLine();
 
+            System.out.print("Address: ");
+            String address = scanner.nextLine();
+
+            System.out.print("Department: ");
+            String department = scanner.nextLine();
+
+            System.out.print("Subjects Taught: ");
+            String subjectsTaught = scanner.nextLine();
+
+            teachers[i] = new Teacher(empId, name, salary, address, department, subjectsTaught);
+        }
+
+        scanner.close();
+ System.out.println("\nDetails of Teachers:");
+        
+        for (Teacher teacher : teachers) {
+            teacher.display();
+        }
+    }
 }
